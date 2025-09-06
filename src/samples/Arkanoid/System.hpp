3f9577d114bea160b59138e6152f239e91ba76c0
Максим Пушкарев <155516836+pushkarev-m@users.noreply.github.com>
2025-09-06 12:57:20 +0400

    added: system templates

--- src/samples/Arkanoid/System.hpp
+++ src/samples/Arkanoid/System.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <cmath>
+#include <map>
 #include <Arkanoid/Object.hpp>
 
 namespace Sample::Arkanoid
@@ -37,4 +38,50 @@ namespace Sample::Arkanoid
 		virtual void OnViewportUpdated() = 0;
 		virtual void OnRender() = 0;
 	};
+
+	template<typename TObject>
+	class ObjectSystem : public virtual BaseSystem
+	{
+		static_assert(std::is_base_of_v<BaseObject, TObject>);
+	public:
+		void TryAttachObject(BaseObject* object) override;
+		void TryDetachObject(BaseObject::IdType id) override;
+	protected:
+		std::map<BaseObject::IdType, TObject*> _objectsMap;
+	};
+
+	template <typename ...TObjects>
+	class MultiObjectSystem : public ObjectSystem<TObjects>...
+	{
+	public:
+		void TryAttachObject(BaseObject* object) override;
+		void TryDetachObject(BaseObject::IdType id) override;
+	};
+
+	template <typename TObject>
+	void ObjectSystem<TObject>::TryAttachObject(BaseObject* object)
+	{
+		if (const auto castedObject = dynamic_cast<TObject*>(object))
+		{
+			_objectsMap.emplace(object->GetId(), castedObject);
+		}
+	}
+
+	template <typename TObject>
+	void ObjectSystem<TObject>::TryDetachObject(BaseObject::IdType id)
+	{
+		_objectsMap.erase(id);
+	}
+
+	template <typename... TObjects>
+	void MultiObjectSystem<TObjects...>::TryAttachObject(BaseObject* object)
+	{
+		(ObjectSystem<TObjects>::TryAttachObject(object), ...);
+	}
+
+	template <typename... TObjects>
+	void MultiObjectSystem<TObjects...>::TryDetachObject(BaseObject::IdType id)
+	{
+		(ObjectSystem<TObjects>::TryDetachObject(id), ...);
+	}
 }
